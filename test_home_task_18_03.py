d769c2db1ed5ee3f07f03cde278c6a4684501ed9
Max_Razzhivin <max.nvo06@gmail.com>
2025-02-10 23:36:14 +0300

    home task 18_03 complete

--- test_home_task_18_03.py
+++ test_home_task_18_03.py
@@ -1,4 +1,6 @@
-
+import random
+from math import pi
+from random import sample
 
 def test_greeting():
     """
@@ -22,12 +24,12 @@ def test_rectangle():
     a = 10
     b = 20
     # TODO сосчитайте периметр
-    perimeter = 0
+    perimeter = (a + b) * 2
 
     assert perimeter == 60
 
     # TODO сосчитайте площадь
-    area = 0
+    area = a * b
 
     assert area == 200
 
@@ -37,14 +39,15 @@ def test_circle():
     Напишите программу, которая берет радиус круга и выводит на экран его длину и площадь.
     Используйте константу PI
     """
+
     r = 23
     # TODO сосчитайте площадь
-    area = 0
+    area = pi * r ** 2
 
     assert area == 1661.9025137490005
 
     # TODO сосчитайте длину окружности
-    length = 0
+    length = 2 * pi * r
 
     assert length == 144.51326206513048
 
@@ -53,8 +56,12 @@ def test_random_list():
     """
     Создайте список из 10 случайных чисел от 1 до 100 (включая обе границы) и отсортируйте его по возрастанию.
     """
+
     # TODO создайте список
-    l = []
+
+
+    l = sample(range(1,101), 10)
+    l.sort()
 
     assert len(l) == 10
     assert all(l[i] <= l[i + 1] for i in range(len(l) - 1))
@@ -66,6 +73,8 @@ def test_unique_elements():
     """
     l = [1, 2, 3, 4, 5, 5, 5, 6, 7, 8, 8, 9, 10, 10]
     # TODO удалите повторяющиеся элементы
+
+    l = list(set(l))
 
     assert isinstance(l, list)
     assert len(l) == 10
@@ -81,7 +90,7 @@ def test_dicts():
     first = ["a", "b", "c", "d", "e"]
     second = [1, 2, 3, 4, 5]
     # TODO создайте словарь
-    d = {}
+    d = dict(zip(first, second))
 
     assert isinstance(d, dict)
     assert len(d) == 5
